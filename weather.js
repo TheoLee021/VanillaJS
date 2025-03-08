3278929ed6f418b84bb4774897885ad1102d4dc9
Theo Lee <theolee021@gmail.com>
2025-03-08 15:37:27 -0800

    Update weather

--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ function onGeoSuccess(position) {
     fetch(url).then((response) => response.json()).then((data) => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+        weather.innerText = `${data.weather[0].main} ${data.main.temp}℃`;
         city.innerText = data.name;
     })
 }
